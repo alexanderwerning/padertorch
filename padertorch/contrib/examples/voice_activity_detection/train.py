db3b657d92c2e55ebc5e3c4e102c4a2996ccbeb7
Alexander Werning <awerning@mail.upb.de>
2020-10-05 09:59:52 +0200

    fix audio padding

--- padertorch/contrib/examples/voice_activity_detection/train.py
+++ padertorch/contrib/examples/voice_activity_detection/train.py
@@ -110,7 +110,8 @@ def prepare_dataset(dataset, audio_segmentation, shuffle=False, batch_size=8, bu
         ex['audio_stop_samples'] = min(ex['audio_stop_samples'], ex['num_samples'])
         ex = audio_reader(ex)
         padded_audio_data = np.zeros((padding_front+ex_num_samples+padding_back))
-        ex['audio_data'] = padded_audio_data[padding_front:-padding_back] = ex['audio_data'].flatten()
+        padded_audio_data[padding_front:-padding_back] = ex['audio_data'].flatten()
+        ex['audio_data'] = padded_audio_data
 
         return ex
 
