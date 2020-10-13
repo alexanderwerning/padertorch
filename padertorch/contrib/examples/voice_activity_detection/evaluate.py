315005beeb26a3cde3d507d8adcb889d8c0be040
Alexander Werning <awerning@mail.upb.de>
2020-10-13 16:55:46 +0200

    fix

--- padertorch/contrib/examples/voice_activity_detection/evaluate.py
+++ padertorch/contrib/examples/voice_activity_detection/evaluate.py
@@ -86,11 +86,11 @@ def get_model_output(ex, model, per_sample, db):
 
     for i, prediction in enumerate(predictions):
         if i < len(predictions)-1:
-            prediction = prediction[BUFFER_SIZE:-BUFFER_SIZE-STFT_SHIFT//2]
+            predictions[i] = prediction[BUFFER_SIZE:-BUFFER_SIZE-STFT_SHIFT//2]
             cumulated_samples += prediction.shape[0]
         else:
             stop = BUFFER_SIZE + ex['num_samples'] - cumulated_samples
-            prediction = prediction[BUFFER_SIZE:stop]
+            predictions[i] = prediction[BUFFER_SIZE:stop]
     print(list(map(lambda x: x.shape, predictions)))
     return predictions
 
