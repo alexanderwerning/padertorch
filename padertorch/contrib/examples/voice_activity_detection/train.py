9d275955a6ffc6ebf6cb7109ff36acbfb9933e86
Alexander Werning <awerning@mail.upb.de>
2021-01-11 16:24:45 +0100

    Comment prepare_dataset and use get_segment_boundaries function

--- padertorch/contrib/examples/voice_activity_detection/train.py
+++ padertorch/contrib/examples/voice_activity_detection/train.py
@@ -4,33 +4,34 @@ Example call:
 export STORAGE_ROOT=<your desired storage root>
 python -m padertorch.contrib.examples.voice_activity_detection.train
 """
-import random
 import json
 import os
-from pathlib import Path
+import random
 from copy import deepcopy
+from pathlib import Path
 
-from sacred import Experiment
-from sacred.observers.file_storage import FileStorageObserver
-from sacred import SETTINGS
-SETTINGS.CONFIG.READ_ONLY_CONFIG = False
-
+import lazy_dataset
 import numpy as np
-from paderbox.utils.timer import timeStamped
+import soundfile
+import torch
+from einops import rearrange
+from paderbox.array import segment_axis
 from paderbox.transform.module_stft import STFT
+from paderbox.utils.timer import timeStamped
 from padercontrib.database.fearless import Fearless
 from padertorch import Trainer
-from padertorch.io import get_new_storage_dir, dump_config, load_config
-from padertorch.contrib.examples.voice_activity_detection.model import SAD_Classifier
+from padertorch.contrib.examples.voice_activity_detection.model import \
+    SAD_Classifier
 from padertorch.contrib.je.modules.conv import CNN1d, CNN2d
-from padertorch.train.optimizer import Adam
+from padertorch.data.segment import get_segment_boundaries
 from padertorch.data.utils import collate_fn
-import torch
+from padertorch.io import dump_config, get_new_storage_dir, load_config
+from padertorch.train.optimizer import Adam
+from sacred import SETTINGS, Experiment
+from sacred.observers.file_storage import FileStorageObserver
 from torch.nn import MaxPool2d
-from paderbox.array import segment_axis
-from einops import rearrange
-import lazy_dataset
-import soundfile
+
+SETTINGS.CONFIG.READ_ONLY_CONFIG = False
 
 experiment_name = "sad"
 experiment = Experiment(experiment_name)
@@ -38,6 +39,7 @@ experiment = Experiment(experiment_name)
 
 def get_model_config():
     return load_config("default_model_config.json")
+
 
 @experiment.config
 def config():
@@ -52,7 +54,8 @@ def config():
     }
     trainer_config["model"] = get_model_config()
     if "storage_dir" not in trainer_config:
-        trainer_config["storage_dir"] = get_new_storage_dir(experiment_name, id_naming="time")
+        trainer_config["storage_dir"] = get_new_storage_dir(
+            experiment_name, id_naming="time")
 
     stft_params = {
         'shift': 80,
@@ -65,7 +68,6 @@ def config():
 
     debug = False
     batch_size = 64
-    batches_buffer = 4
     train_chunk_size = 4 * sample_rate
     validate_chunk_size = 30 * sample_rate
 
@@ -79,12 +81,14 @@ def config():
 def debug_dataset(dataset):
     """Create a dataset containing only the first element of the given dataset."""
     first_example = dataset[1]  # this example does not immediately start with activity
-    dict_dataset = {index: first_example.copy() for index in range(len(dataset))}
+    dict_dataset = {index: first_example.copy()
+                    for index in range(len(dataset))}
     return lazy_dataset.new(dict_dataset)
 
 
 @experiment.capture
 def get_datasets(data_subset, train_chunk_size, validate_chunk_size, stft_params, batch_size, batches_buffer, debug):
+    """Initialize the dataset objects."""
     db = Fearless()
     train_set = db.get_dataset_train(subset=data_subset)
     validate_set = db.get_dataset_validation(subset=data_subset)
@@ -93,35 +97,26 @@ def get_datasets(data_subset, train_chunk_size, validate_chunk_size, stft_params
         train_set = debug_dataset(train_set)
         validate_set = debug_dataset(validate_set)
 
-    training_data = prepare_dataset(train_set, chunker, stft_params, shuffle=True, batch_size=batch_size, batches_buffer=batches_buffer, train=True)
-    validation_data = prepare_dataset(validate_set, select_speech, stft_params, batch_size=8, batches_buffer=batches_buffer)
+    training_data = prepare_dataset(
+        train_set, chunker, stft_params, shuffle=True, batch_size=batch_size, train=True)
+    validation_data = prepare_dataset(
+        validate_set, select_speech, stft_params, batch_size=8)
     return training_data, validation_data
 
 
 @experiment.capture
 def chunker(example, train_chunk_size, debug):
-    """Cut out a random 4s segment from the stream for training."""
-
-    start = max(0, np.random.randint(example['num_samples'])-train_chunk_size)
-
-    if debug:
-        debug_example = select_speech(example, train_chunk_size)
-        examples = [deepcopy(debug_example) for _ in range((example['num_samples']-start)//train_chunk_size)]
-        return examples
-
+    """Cut a batch of 4s segments from the stream for training."""
     examples = []
 
-    for _ in range((example['num_samples']-start)//train_chunk_size):
+    boundaries = get_segment_boundaries(example['num_samples'], train_chunk_size, train_chunk_size, anchor='random')
+
+    for start, stop in np.nditer(boundaries, op_axes=0):
         example_chunk = example.copy()
-        stop = start + train_chunk_size
         example_chunk.update(audio_start_samples=start)
         example_chunk.update(audio_stop_samples=stop)
         example_chunk.update(activity=example['activity'][start:stop])
-        if debug and len(examples) > 0:
-            examples.append[examples[0]]
-        else:
-            examples.append(example_chunk)
-        start = stop
+        examples.append(example_chunk)
 
     random.shuffle(examples)
 
@@ -135,7 +130,7 @@ def select_speech(example, validate_chunk_size, sample_rate, debug, time_buffer_
     We evaluate the model on 30s audio segments which contain speech.
     """
     first_speech = example['activity'].intervals[0][0]
-    
+
     if not debug:
         max_time_buffer = sample_rate * time_buffer_sec  # 1s
         time_buffer = np.random.randint(max_time_buffer)
@@ -150,46 +145,78 @@ def select_speech(example, validate_chunk_size, sample_rate, debug, time_buffer_
     return example
 
 
-def prepare_dataset(dataset, audio_segmentation, stft_params, shuffle=False, batch_size=8, batches_buffer=4, num_workers=6, train=False, audio_reader=None):
+def prepare_dataset(dataset, audio_segmentation, stft_params, shuffle=False, batch_size=8, num_workers=6, train=False, audio_reader=None):
+    """Apply transformations to a dataset so it can be used for training a model.
+
+        Args:
+            dataset:
+                The dataset object on which to apply the transformations.
+            audio_segmentation:
+                A function which reduces the audio stream to a small segment or a batch of segments.
+            stft_params:
+                The parameters used for the STFT.
+            shuffle:
+                Whether to shuffle the streams before applying the 'audio_segmentation'. Note that if a stream is segmented into multiple segments, these need to be shuffled by the segmentation function.
+            batch_size:
+                The size of the resulting batches.
+            num_workers:
+            train:
+                Whether this is a training or a validation dataset. It is assumed that in training 'audio_segmentation' produces batches, whereas in validation it returns single segments per example.
+            audio_reader:
+                A function to read the audio files. If set to 'None', a default is used, which reads the given 'audio_path' from 'audio_start_samples' to 'audio_stop_samples' into 'audio_data'.
+        Returns:
+            dataset:
+                The dataset with the transformations added.
+        """
 
     def prepare_example(example):
+        """Simplify audio_path attribute and load activity data for an example."""
         example['audio_path'] = example['audio_path']['observation']
         example['activity'] = Fearless.get_activity(example)
         return example
 
     dataset = dataset.map(prepare_example)
 
+    # shuffle the streams
     if shuffle:
         dataset = dataset.shuffle(reshuffle=True)
 
+    # create batches with segments from the streams
+    # when evaluating, this does not result in batches, but single examples
     dataset = dataset.map(audio_segmentation)
 
+    # unbatch if we are training (otherwise there are no batches)
     if train:
         dataset = dataset.unbatch()
 
-    buffer_size = int(batches_buffer * batch_size)
-
+    # default audio reader
     def read_audio(example):
+        """Read the audio from the given path according to the selected samples."""
         audio_path = str(example["audio_path"])
         start_samples = example["audio_start_samples"]
         stop_samples = example["audio_stop_samples"]
 
-        x, sr = soundfile.read(audio_path, start=start_samples, stop=stop_samples)
+        x, sr = soundfile.read(
+            audio_path, start=start_samples, stop=stop_samples)
         audio = x.T
         example["audio_data"] = audio
         return example
+
+    # read audio file into examples
     if audio_reader is None:
         dataset = dataset.map(read_audio)
     else:
         dataset = dataset.map(lambda ex: audio_reader(ex, read_audio))
 
+    # apply stft to audio
     stft = STFT(**stft_params)
 
     def calculate_stft(example):
+        """Calculate the STFT spectrum."""
         complex_spectrum = stft(example['audio_data'])
         real_magnitude = (np.abs(complex_spectrum)**2).astype(np.float32)
         features = rearrange(real_magnitude[None, None, ...],
-                             'b c t f -> b c f t', c=1, b=1)[:, :, :, :]
+                             'b c t f -> b c f t', c=1, b=1)[:, :, :, :]  # reorder the dimensions to batch | channel | frequency | time
         example['features'] = features
         example['activity_samples'] = example['activity'][:]
         example['activity'] = segment_axis(example['activity'][:],
@@ -201,6 +228,7 @@ def prepare_dataset(dataset, audio_segmentation, stft_params, shuffle=False, bat
 
     dataset = dataset.map(calculate_stft)
 
+    # renaming and typecasting
     def finalize(example):
         return {
             'example_id': example['example_id'],
@@ -212,6 +240,7 @@ def prepare_dataset(dataset, audio_segmentation, stft_params, shuffle=False, bat
 
     dataset = dataset.map(finalize)
 
+    # create batches and stack vectors
     dataset = dataset.batch(batch_size).map(collate_fn)
 
     def unpack_tensor(batch):
@@ -227,6 +256,7 @@ def prepare_dataset(dataset, audio_segmentation, stft_params, shuffle=False, bat
 
 @experiment.capture
 def get_trainer(trainer_config, load_model_from):
+    """Initialize the PaderTorch Trainer object."""
     trainer = Trainer.from_config(trainer_config)
 
     checkpoint_path = trainer.checkpoint_dir / 'ckpt_latest.pth'
