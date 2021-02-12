776b16becc50fd375cccc4a00741126aa6aaab43
Alexander Werning <awerning@mail.upb.de>
2021-02-12 10:40:15 +0100

    fix

--- padertorch/contrib/examples/voice_activity_detection/segment.py
+++ padertorch/contrib/examples/voice_activity_detection/segment.py
@@ -9,7 +9,7 @@ def activity_frequency_to_time(
         stft_shift,
         time_length=None,
 ):
-    frequency_activity.expand((*frequency_activity.size(), stft_window_length))
+    frequency_activity.expand(*(*frequency_activity.size(), stft_window_length))
 
     time_activity = torch.zeros(
         (*frequency_activity.size()[:-2],
