6c8cd45d642e46582f3a1accb6d80189a3e38c38
Alexander Werning <awerning@mail.upb.de>
2021-02-12 10:17:56 +0100

    fix

--- padertorch/contrib/examples/voice_activity_detection/evaluate.py
+++ padertorch/contrib/examples/voice_activity_detection/evaluate.py
@@ -16,7 +16,56 @@ from padertorch.contrib.examples.voice_activity_detection.train import (
 from padertorch.contrib.jensheit.eval_sad import evaluate_model, smooth_vad
 from padertorch.data import example_to_device
 from padertorch.io import load_config
-from padertorch.contrib.examples.voice_activity_detection.segment import activity_frequency_to_time
+
+# adapted from padercontrib.database.chime5.database
+def activity_frequency_to_time(
+        frequency_activity,
+        stft_window_length,
+        stft_shift,
+        time_length=None,
+):
+
+    frequency_activity = np.asarray(frequency_activity)
+
+    frequency_activity = np.broadcast_to(
+        frequency_activity[..., None], (*frequency_activity.shape, stft_window_length)
+    )
+
+    time_activity = np.zeros(
+        (*frequency_activity.shape[:-2],
+         frequency_activity.shape[-2] * stft_shift + stft_window_length - stft_shift)
+    )
+
+    # Get the correct view to time_signal
+    time_signal_seg = segment_axis(
+        time_activity, stft_window_length, stft_shift, end=None
+    )
+
+    time_signal_seg[:] = frequency_activity
+
+    if time_length is not None:
+        if time_length == time_activity.shape[-1]:
+            pass
+        elif time_length < time_activity.shape[-1]:
+            delta = time_activity.shape[-1] - time_length
+            assert delta < stft_window_length - stft_shift, (delta, stft_window_length, stft_shift)
+            time_activity = time_activity[..., :time_length]
+
+        elif time_length > time_activity.shape[-1]:
+            delta = time_length - time_activity.shape[-1]
+            assert delta < stft_window_length - stft_shift, (delta, stft_window_length, stft_shift)
+
+            time_activity = pad_axis(
+                time_activity,
+                pad_width=(0, delta),
+                axis=-1,
+            )
+        else:
+            raise Exception('Can not happen')
+        assert time_length == time_activity.shape[-1], (time_length, time_activity.shape)
+
+    return time_activity
+
 
 experiment = sacred.Experiment('VAD Evaluation')
 
--- padertorch/contrib/examples/voice_activity_detection/segment.py
+++ padertorch/contrib/examples/voice_activity_detection/segment.py
@@ -1,4 +1,4 @@
-import numpy as np
+import torch
 from paderbox.array import segment_axis
 from paderbox.array.padding import pad_axis
 
@@ -10,15 +10,14 @@ def activity_frequency_to_time(
         time_length=None,
 ):
 
-    frequency_activity = np.asarray(frequency_activity)
 
-    frequency_activity = np.broadcast_to(
-        frequency_activity[..., None], (*frequency_activity.shape, stft_window_length)
+    frequency_activity = torch.broadcast_to(
+        frequency_activity[..., None], (*frequency_activity.size(), stft_window_length)
     )
 
-    time_activity = np.zeros(
-        (*frequency_activity.shape[:-2],
-         frequency_activity.shape[-2] * stft_shift + stft_window_length - stft_shift)
+    time_activity = torch.zeros(
+        (*frequency_activity.size()[:-2],
+         frequency_activity.size()[-2] * stft_shift + stft_window_length - stft_shift)
     )
 
     # Get the correct view to time_signal
@@ -29,15 +28,15 @@ def activity_frequency_to_time(
     time_signal_seg[:] = frequency_activity
 
     if time_length is not None:
-        if time_length == time_activity.shape[-1]:
+        if time_length == time_activity.size()[-1]:
             pass
-        elif time_length < time_activity.shape[-1]:
-            delta = time_activity.shape[-1] - time_length
+        elif time_length < time_activity.size()[-1]:
+            delta = time_activity.size()[-1] - time_length
             assert delta < stft_window_length - stft_shift, (delta, stft_window_length, stft_shift)
             time_activity = time_activity[..., :time_length]
 
-        elif time_length > time_activity.shape[-1]:
-            delta = time_length - time_activity.shape[-1]
+        elif time_length > time_activity.size()[-1]:
+            delta = time_length - time_activity.size()[-1]
             assert delta < stft_window_length - stft_shift, (delta, stft_window_length, stft_shift)
 
             time_activity = pad_axis(
@@ -47,6 +46,6 @@ def activity_frequency_to_time(
             )
         else:
             raise Exception('Can not happen')
-        assert time_length == time_activity.shape[-1], (time_length, time_activity.shape)
+        assert time_length == time_activity.size()[-1], (time_length, time_activity.size())
 
     return time_activity
